919c394018a850c59ded947eb739bb9e2bf72736
Krevedka <ailarridionov@yandex.ru>
2026-08-12 10:57:04 +0500

    Update user.nix

--- modules/system/user.nix
+++ modules/system/user.nix
@@ -2,8 +2,7 @@
 {
   users.users.${username} = {
     isNormalUser = true; # Standart user, not root
-    description = "Krevedka"; # Name Krevedka lol
-    initialPassword = "nixos1234";
+    description = "RandomGitHubUser1234"; # Enter your full name or nickname
     extraGroups = [ "networkmanager" "wheel" "video" "libvirtd" "audio" "input" "kvm" ];
     shell = pkgs.fish; # Default shell fish
     packages = with pkgs; [
