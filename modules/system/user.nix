b77b9798bf35a24060d2d1334a1cabb1ca9c1549
Krevedka <ailarridionov@yandex.ru>
2026-08-12 15:51:21 +0500

    Update user.nix

--- modules/system/user.nix
+++ modules/system/user.nix
@@ -3,6 +3,7 @@
   users.users.${username} = {
     isNormalUser = true; # Standart user, not root
     description = "RandomGitHubUser1234"; # Enter your full name or nickname
+    initialPassword = "nixos1234"; # Delete this line, after installation and login! Change password via "passwd"!
     extraGroups = [ "networkmanager" "wheel" "video" "libvirtd" "audio" "input" "kvm" ];
     shell = pkgs.fish; # Default shell fish
     packages = with pkgs; [
