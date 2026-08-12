1141f2b4edc9f2c281e0bec5c798eaf0e99c89b2
Krevedka <ailarridionov@yandex.ru>
2026-08-12 16:03:12 +0500

    Update nvidia.nix

--- modules/system/nvidia.nix
+++ modules/system/nvidia.nix
@@ -15,8 +15,8 @@
     nvidiaSettings = true;
 
   # Power manager for laptops
-    powerManagement.enable = false;
-    powerManagement.finegrained = false;
+    powerManagement.enable = false; # Change "true", if you have laptop
+    powerManagement.finegrained = false; # This too...
 
   # Open drivers ONLY for 16xx or RTX series of NOvidea cards
     open = true; # if 10xx or older "= false";
